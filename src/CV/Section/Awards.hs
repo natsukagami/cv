717ed586fbb85d743b977e2b785a8dd2df993aef
Natsu Kagami <nki@nkagami.me>
2021-06-27 03:40:44 +0900

    Move html source to Haskell

---
+++ src/CV/Section/Awards.hs
@@ -0,0 +1,89 @@
+module CV.Section.Awards (awards) where
+
+import CV.Prelude
+import CV.Section as CV
+import Protolude
+import Text.Blaze.Html5 as B
+import qualified Text.Blaze.Html5.Attributes as A
+
+awards :: Html
+awards = CV.section "Awards and Scholarships" $ mapM_ award awards
+  where
+    awards =
+      [ -- IOI
+        Award
+          (Link "http://stats.ioinformatics.org/people/6056" "International Olympiad in Informatics")
+          [ Badge "http://stats.ioinformatics.org/results/2016" "🥇" (badgeFmt "Russia 2016" "Gold Medal"),
+            Badge "http://stats.ioinformatics.org/results/2017" "🥉" (badgeFmt "Iran 2017" "Bronze Medal")
+          ]
+          [ "Participated in the world's most prestigious programming competition for high school students.",
+            "Placed in top 30 out of more than 300 participants."
+          ],
+        -- ACM ICPC
+        Award
+          ( Link "https://icpc.global/worldfinals/teams" $
+              B.div $ do
+                acmICPC >> " - World Finals"
+                div_ "text-sm text-gray-600" $ "team: " >> B.b "unsigned"
+          )
+          [Badge "https://icpc.global/community/results-2018" "🌏 #14" (badgeFmt "Beijing 2018" "")]
+          [ "Participated in the world's most prestigious programming competition for university students.",
+            "Placed in top 15 out of more than 100 teams."
+          ],
+        -- Scholarship
+        Award
+          (Link "https://uwaterloo.ca/news/news/mike-and-ophelia-lazaridis-pledge-21-million-science-and" $ B.span ! A.title "Original scholarship link is down" $ scholarship)
+          [Badge "" "💰" "$200,000"]
+          [ "Received full scholarship for all academic terms.",
+            "One of the only four receipients of the 2018 scholarship."
+          ]
+      ]
+    acmICPC = do
+      span_ "lg:inline hidden" "ACM International Collegiate Programming Contest"
+      span_ "lg:hidden" ! A.title "ACM International Collegiate Programming Contest" $ "ACM ICPC"
+    scholarship = do
+      span_ "lg:inline hidden" "Michael and Ophelia Lazaridis Olympiad Scholarship"
+      div_ "lg:hidden" $ do
+        div_ "" "Olympiad Scholarship"
+        div_ "text-sm text-gray-600" $ do
+          "Michael and Ophelia Lazaridis Olympiad Scholarship"
+          div_ "md:hidden" "Estimated Amount $200,000"
+    badgeFmt location value = do
+      span_ "text-indigo-800" location
+      span_ "lg:inline hidden" $ do
+        " - "
+        value
+
+data Award = Award
+  { awardTitle :: Link,
+    awardBadges :: [Badge],
+    awardDesc :: [Html]
+  }
+
+data Badge = Badge
+  { badgeLink :: Text,
+    badgeIcon :: Html,
+    badgeText :: Html
+  }
+
+award :: Award -> Html
+award Award {..} = B.section ! card_ "" $ do
+  div_ "flex flex-row justify-between items-center" $ do
+    -- Title
+    B.div ! cardTitle_ "" $ link_ "hover:text-blue-600" awardTitle
+    -- Badges
+    div_ "flex flex-row space-x-2" $ mapM_ badge awardBadges
+  B.ul ! c_ "list-inside list-disc" $ mapM_ B.li awardDesc
+
+badge :: Badge -> Html
+badge Badge {..} = ifLink $
+  div_ "group rounded-full p-2 border hover:shadow-xl hover:border-blue-600 transition-shadow" $ do
+    -- Only show text on hover + sm-screens, or large screens
+    span_ "sm:group-hover:inline lg:inline hidden" badgeText
+    -- Always show icon
+    badgeIcon
+    pure ()
+  where
+    ifLink = case badgeLink of
+      "" -> identity
+      _ -> link_ "" . Link (toAttr badgeLink)
---
+++ src/CV/Section/Work.hs
@@ -0,0 +1,99 @@
+module CV.Section.Work (workExperience, personalProjects) where
+
+import CV.Prelude
+import CV.Section as CV
+import Protolude
+import Text.Blaze.Html5 as B
+import qualified Text.Blaze.Html5.Attributes as A
+
+workExperience :: Html
+workExperience = CV.section "Work Experience" $ mapM_ job_ jobs
+  where
+    jobs = [dotty, multibaas]
+    dotty =
+      Job
+        (Link "https://github.com/lampepfl/dotty" "Dotty")
+        "The official Scala 3 Compiler"
+        "Part-time Contributor"
+        Nothing
+        "January - April 2021"
+        [ "Contributed as part of University of Waterloo's " >> B.i "Undergraduate Research Assistant" >> " program.",
+          "Improved the initialization checker of the" >> hoverB "Scala 3" >> "compiler on early promotion cases.",
+          "Reduced the number of false-positions by the checker by 40% on common codebases."
+        ]
+    multibaas =
+      Job
+        (Link "https://curvegrid.com" "Curvegrid MultiBaas")
+        "Software development playform on Ethereum Blockchain"
+        "Blockchain Software Engineer"
+        (Just "Tokyo, Japan")
+        "Summer 2019-2021"
+        [ "Developed a friendly REST API over the Blockchain API using " >> hoverB "Go," >> "deployable as a service.",
+          "Designed an intuitive Blockchain and Smart Contracts management UI in " >> hoverB "Vue.",
+          "Contributed bugfixes and new features to the" >> hoverB "go‑ethereum and OpenAPI" >> "open source repositories."
+        ]
+
+personalProjects :: Html
+personalProjects = CV.section "Personal Projects" $ mapM_ job_ projects
+  where
+    projects = [codefun, kjudge]
+    codefun =
+      Job
+        (Link "https://codefun.vn" "Codefun.vn")
+        "A Programming Practice website for Students"
+        "Website Creator and Maintainer"
+        Nothing
+        "2015 - Present"
+        [ "Created a website that hosts programming challenges and automatically judges codes based on case-testing.",
+          "Integrated modern architecture with REST API using " >> hoverB "Go" >> " and " >> hoverB "Rust;" >> "serving thousands of connections in milliseconds.",
+          "Leveraged" >> hoverB "React" >> "for a smooth single page application experience.",
+          "Used currently by" >> hoverB "5000+ high-school and university students" >> "in Vietnam."
+        ]
+    kjudge =
+      Job
+        (Link "https://github.com/natsukagami/kjudge" "kjudge")
+        "Minimal Platform for Competitive Programming contests"
+        "Creator and Maintainer"
+        Nothing
+        "2020 - Present"
+        [ "Written completely in" >> hoverB "Go" >> "for a simple one-binary service.",
+          "Leverages" >> hoverB "Docker" >> "for an integrated one-command deployment.",
+          "Supports multiple competitive programming styles (IOI, ICPC) with flexibility in problem design.",
+          "Used in many local programming contests in Vietnam as a replacement for the " >> link_ "hover:text-blue-400 text-blue-600" (Link "https://github.com/ioi/cms" "official IOI platform.")
+        ]
+
+data Job = Job
+  { jobTitle :: Link,
+    jobSubtitle :: Html,
+    jobPosition :: Html,
+    jobLocation :: Maybe Html,
+    jobTime :: Html,
+    jobDesc :: [Html]
+  }
+
+job_ :: Job -> Html
+job_ Job {..} = B.section ! card_ "group" $ header >> main
+  where
+    header = B.header $ do
+      -- Job title and Subtitle
+      h3 ! cardTitle_ "space-x-2 flex flex-row items-baseline" $ do
+        link_ "hover:text-blue-600" jobTitle
+        div_ "text-gray-600 text-base" jobSubtitle
+      -- Job position and location/time
+      div_ "flex flex-row text-sm text-gray-800" $ do
+        -- Job position
+        div_ "pr-2 uppercase" jobPosition
+        div_ "block flex-grow" $ pure () -- Splitter
+        div_ "text-indigo-800" $ do
+          -- Job location, if exists
+          case jobLocation of
+            Just loc -> span_ "px-2 border-r-2" loc
+            Nothing -> pure ()
+          span_ "px-2" jobTime
+    main =
+      B.main $
+        B.ul ! c_ "list-disc list-inside" $
+          mapM_ B.li jobDesc
+
+hoverB :: Html -> Html
+hoverB = span_ "group-hover:font-bold"
